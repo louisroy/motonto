51e0e0d1687f38a48040af102b7ad71719fcfbee
Louis Roy <louis.roy@cossette.com>
2015-07-27 15:51:02 -0400

    better

--- index.js
+++ index.js
@@ -12,34 +12,71 @@ var creds = {
 	private_key:process.env.PRIVATE_KEY
 };
 
-var locationId = 1700273; // Toronto
-var categoryId = 30; // Motorcycles
+var locations = [ 1700273 ]; // Toronto
+var categories = [ 303, 304, 305, 306, 307, 308 ] ; // Motorcycles
+var existingGuids = [];
 
 sheet.useServiceAccountAuth(creds, function(err) {
-	sheet.getInfo(function(err, sheetInfo){
-		fetchAds();
-	});
+	if (err) {
+		return console.log(err);
+	}
+	
+	sheet.getInfo(fetchExistingGuids);
 });
 
-var fetchAds = function() {
+var fetchExistingGuids = function(err, sheetInfo) {
+	if (err) {
+		return console.log(err);
+	}
+	
+	sheet.getCells(1, { 'min-row':1, 'max-row':1000, 'min-col':7, 'max-col':7 }, function(err, cells) {
+		if (err) {
+			return console.log(err);
+		}
+		
+		_.each(cells, function(cell) {
+			existingGuids.push(cell.value);
+		});
+		
+		fetchAllAds();
+	});
+}
+
+var fetchAllAds = function() {
+	_.each(locations, function(locationId) {
+		_.each(categories, function(categoryId) {
+			fetchAds(locationId, categoryId);
+		});
+	});
+}
+
+var fetchAds = function(locationId, categoryId) {
+	console.log('Fetching category #%d for location #%d, locationId, categoryId');
+	
 	var prefs = {
 		locationId:locationId,
 		categoryId:categoryId
 	};
 	
 	var params = {
-		minPrice:0,
-		maxPrice:1000,
 		adType:'OFFER'
 	};
 
 	kijiji.query(prefs, params, function(err, ads) {
+		if (err) {
+			return console.log(err);
+		}
+		
 		writeToFile(ads);
 	});
 }
 
 var writeToFile = function(ads) {
 	_.each(ads, function(ad, index, list) {
+		if (existingGuids.indexOf(ad.guid) !== -1) {
+			return console.log('found existing guid');
+		}
+		
 		var row = {
 			guid:ad.guid,
 			title:ad.title,
@@ -53,11 +90,16 @@ var writeToFile = function(ads) {
 		
 		for (var key in row) {
 			if (row[key] == null) {
-				console.log('null found', row);
 				return true;
 			}
 		}
 		
-		sheet.addRow(1, row);
+		sheet.addRow(1, row, function(err) {
+			if (err) {
+				return console.log(err);
+			}
+			
+			console.log('successfully added row' );
+		});
 	});
 }
