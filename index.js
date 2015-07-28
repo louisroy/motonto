f36f250165738739623e2a1d45a1e474c612f5a5
Louis Roy <louis.roy@cossette.com>
2015-07-28 14:53:47 -0400

    better log

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var server = app.listen(process.env.PORT || 5000, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 	
-	console.log('Example app listening at http://%s:%s', host, port);
+	console.log('App listening at http://%s:%s', host, port);
 });
 
 var init = function(callback) {
