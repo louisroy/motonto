d1303c544c08783772d09b8674cd62eeb2a80f06
Louis Roy <louis.roy@cossette.com>
2015-07-28 09:06:49 -0400

    async extravaganza

--- index.js
+++ index.js
@@ -14,18 +14,25 @@ var sheet, creds, locations, categories,existingGuids;
 
 // Web server
 app.get('/', function (req, res) {
-	initializeScraper();
-	res.send('Hello World!');
+	init(function(err, result) {
+		res.send('Hello World!');
+	});
 });
 
 var server = app.listen(process.env.PORT || 5000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+	var host = server.address().address;
+	var port = server.address().port;
+	
+	init(function(err, writtenAds) {
+		console.log("Successfully added %s ads to spreadsheet.", writtenAds);
+	});
+	
+	console.log('Example app listening at http://%s:%s', host, port);
 });
 
-var initializeScraper = function() {
+var init = function(callback) {
+	callback = callback || function() {};
+	
 	locations = process.env.LOCATION_IDS.split(','); // Toronto
 	categories = process.env.CATEGORY_IDS.split(','); // Motorcycles
 	existingGuids = [];
@@ -36,42 +43,62 @@ var initializeScraper = function() {
 		private_key:process.env.PRIVATE_KEY
 	};
 	
-	sheet.useServiceAccountAuth(creds, function(err) {
-		if (err) {
-			return console.log(err);
-		}
-		
-		sheet.getInfo(fetchExistingGuids);
-	});
-}
+	async.waterfall([
+		authenticate,
+		fetchExistingGuids,
+		fetchAllAds,
+		writeAds
+	], callback);
+};
 
-var fetchExistingGuids = function(err, sheetInfo) {
-	if (err) {
-		return console.log(err);
-	}
-	
-	sheet.getCells(1, { 'min-row':1, 'max-row':1000, 'min-col':8, 'max-col':8 }, function(err, cells) {
-		if (err) {
-			return console.log(err);
-		}
+var authenticate = function(callback) {
+	sheet.useServiceAccountAuth(creds, function(err) {
+		if (err) return callback(err);
+		
+		sheet.getInfo(function(err, sheetInfo) {
+			if (err) return callback(err);
+			
+			return callback(null, sheetInfo)
+		});
+	});
+};
+
+var fetchExistingGuids = function(sheetInfo, callback) {
+	sheet.getCells(1, { 'min-row':1, 'max-row':1000, 'min-col':9, 'max-col':9 }, function(err, cells) {
+		if (err) return callback(err);
 		
 		_.each(cells, function(cell) {
 			existingGuids.push(cell.value);
 		});
 		
-		fetchAllAds();
+		return callback(null, existingGuids);
 	});
-}
+};
 
-var fetchAllAds = function() {
+var fetchAllAds = function(guids, callback) {
+	var tasks = [];
+	
 	_.each(locations, function(locationId) {
 		_.each(categories, function(categoryId) {
-			fetchAds(locationId, categoryId);
+			tasks.push(function(taskCallback) {
+				fetchAds(locationId, categoryId, taskCallback);
+			});
 		});
 	});
-}
+	
+	// Run fetching tasks in parallel
+	async.parallel(tasks, function(err, adGroups) {
+		if (err) return callback(err);
+		
+		// Flatten array
+		var ads = [];
+			ads = ads.concat.apply(ads, adGroups);
+		
+		callback(null, ads);
+	});
+};
 
-var fetchAds = function(locationId, categoryId) {
+var fetchAds = function(locationId, categoryId, callback) {
 	console.log('Fetching category #%s for location #%s, locationId, categoryId');
 	
 	var prefs = {
@@ -86,46 +113,61 @@ var fetchAds = function(locationId, categoryId) {
 	};
 
 	kijiji.query(prefs, params, function(err, ads) {
-		if (err) {
-			return console.log(err);
-		}
+		if (err) return callback(err);
 		
-		writeToFile(ads);
+		callback(null, ads);
 	});
-}
+};
 
-var writeToFile = function(ads) {
+var writeAds = function(ads, callback) {
+	var totalAds = ads.length;
+	var writtenAds = 0;
+	var onProgress = function() {
+		if (writtenAds >= totalAds) {
+			callback(null, writtenAds);
+		}
+	};
+	
 	_.each(ads, function(ad, index, list) {
+		// Check if ad is already in spreadsheet
 		if (existingGuids.indexOf(ad.guid) !== -1) {
-			return console.log('found existing guid');
+			totalAds--;
+			return onProgress();
 		}
 		
+		// Prepare data
 		var row = {
 			guid:ad.guid,
 			title:ad.title,
 			date:ad['dc:date'],
-			price:ad.innerAd.info.Price,
-			colour:ad.innerAd.info.Colour,
-			kilometers:ad.innerAd.info.Kilometers,
-			make:ad.innerAd.info.Make,
-			model:ad.innerAd.info.Model,
-			year:ad.innerAd.info.Year,
+			price:ad.innerAd.info['Price'],
+			colour:ad.innerAd.info['Colour'],
+			kilometers:ad.innerAd.info['Kilometers'],
+			make:ad.innerAd.info['Make'],
+			model:ad.innerAd.info['Model'],
+			year:ad.innerAd.info['Year'],
+			engine:ad.innerAd.info['Engine Displacement (cc)']
 		};
 		
+		// Filter out ads with missing info
 		for (var key in row) {
 			if (row[key] == null) {
-				return true;
+				totalAds--;
+				return onProgress();
 			}
 		}
 		
+		// Add ad to spreasheet
 		sheet.addRow(1, row, function(err) {
 			if (err) {
-				return console.log(err);
+				totalAds--;
+				
+				// TODO : Trigger callback?
+			} else {
+				writtenAds++;
 			}
 			
-			console.log('successfully added row' );
+			return onProgress();
 		});
 	});
-	
-	console.log('finished process' );
-}
+};
